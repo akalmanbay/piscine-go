86ebef4f7683e6d907e9626ec8992ebd0c4d7cd6
akalmanbay <56753064+akalmanbay@users.noreply.github.com>
2020-02-27 15:51:49 +0600

    Create isalpha.go

---
+++ isalpha.go
@@ -0,0 +1,11 @@
+package piscine
+
+func IsUpper(str string) bool {
+
+	for i, _ := range str {
+		if str[i] > 90 || str[i] < 65 || str[i] > 122 || str[i] < 97 || str[i] == 35 || str[i] == 38 str[i] == 42 || str[i] == 64 {
+			return false
+		}
+	}
+	return true
+}
